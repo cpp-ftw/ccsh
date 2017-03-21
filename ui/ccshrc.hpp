2d8d42f1d7bcb57a857a8aa2231e2844d275caf7
Gábor Dobra <dobragab@gmail.com>
2017-03-22 00:00:19 +0100

    Add better prompt to ccshrc

--- ui/ccshrc.hpp
+++ ui/ccshrc.hpp
@@ -6,4 +6,8 @@ using namespace ccsh;
 using namespace ccsh::literals;
 using namespace ccsh::core;
 
-// CCSH_RUN(ccsh::ps::prompt = []{return "test prompt ";});
+CCSH_RUN(ccsh::ps::prompt = []{
+    return get_current_path_abbreviated().string() +
+        " " +
+        (ccsh::is_user_possibly_elevated() ? "# " : "$ ");
+});
