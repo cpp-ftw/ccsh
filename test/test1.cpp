665a1c23083b7982be8cb23715e672234e178882
Gábor Dobra <dobragab@gmail.com>
2017-04-01 14:53:56 +0200

    Add first gtest suites

---
+++ test/test1.cpp
@@ -0,0 +1,37 @@
+#include <ccsh/ccsh.hpp>
+#include <cstdio>
+#include <string>
+#include <fstream>
+
+#include <gtest/gtest.h>
+
+using namespace ccsh;
+
+std::string ReadAllText(fs::path filename)
+{
+    std::ifstream t(filename.c_str());
+    return std::string(std::istreambuf_iterator<char>(t),
+                       std::istreambuf_iterator<char>());
+}
+
+TEST(NativeTest, CopyRemoveFile)
+{
+    fs::path example_path1{__FILE__};
+    fs::path example_path2 = example_path1.parent_path() / "example1.file";
+
+    std::string example_content1 = ReadAllText(example_path1);
+    EXPECT_NE(example_content1, "");
+
+    shell("rm", {example_path2.string()});
+    EXPECT_EQ(fs::exists(example_path2), false);
+
+    shell("cp", {example_path1.string(), example_path2.string()});
+    EXPECT_EQ(fs::exists(example_path2), true);
+
+    fs::path example_content2 = ReadAllText(example_path2);
+    EXPECT_EQ(example_content1, example_content2);
+
+    shell("rm", {example_path2.string()});
+    EXPECT_EQ(fs::exists(example_path2), false);
+}
+
---
+++ test/test2.cpp
@@ -0,0 +1,13 @@
+#include <ccsh/ccsh.hpp>
+#include <cstdio>
+#include <string>
+#include <fstream>
+
+#include <gtest/gtest.h>
+
+using namespace ccsh;
+
+TEST(DummyTest, Dummy)
+{
+    EXPECT_EQ(true, true);
+}
