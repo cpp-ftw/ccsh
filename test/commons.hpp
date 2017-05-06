4c5300acffbc30f13b6bf929e8b781264222f7d9
Gábor Dobra <dobragab@gmail.com>
2017-05-06 15:05:16 +0200

    Add vector_in_out mapping test

--- test/commons.hpp
+++ test/commons.hpp
@@ -7,6 +7,7 @@
 
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 #include <gtest/gtest.h>
 
--- test/test_mapping.cpp
+++ test/test_mapping.cpp
@@ -14,3 +14,20 @@ TEST(MappingTest, string_in_out)
     EXPECT_EQ(s1, "test");
 }
 
+TEST(MappingTest, vector_in_out)
+{
+    std::size_t no = 100;
+    std::string line = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    std::vector<std::string> vec_in;
+    std::generate_n(std::back_inserter(vec_in), no, [&]{ return line; });
+    ASSERT_EQ(vec_in.size(), no);
+
+    std::vector<std::string> vec_out;
+
+    cat() < vec_in > vec_out;
+
+    EXPECT_EQ(vec_in.size(), 0);
+    EXPECT_EQ(vec_out.size(), no);
+    for(auto const& elem : vec_out)
+        EXPECT_EQ(elem, line);
+}
